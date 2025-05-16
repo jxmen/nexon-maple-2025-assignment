d2a00cd19601a59aa849454a65c586bdd14d5781
jxmen <sprnd645@gmail.com>
2025-05-16 22:40:36 +0900

    fix(auth-server): e2e 테스팅 mongodb-memory-server 코드로 수정

--- auth-server/test/app.e2e-spec.ts
+++ auth-server/test/app.e2e-spec.ts
@@ -1,19 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from '../src/app.controller';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+  let app: INestApplication;
+  let mongod: MongoMemoryServer;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [MongooseModule.forRoot(mongod.getUri())],
+      controllers: [AppController],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+    if (mongod) await mongod.stop();
   });
 
   it('/ (GET)', () => {
