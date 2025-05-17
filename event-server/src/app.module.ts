6cdbb1163aa47337a8b130791ba29298a17d05a2
jxmen <sprnd645@gmail.com>
2025-05-17 23:52:48 +0900

    feat(event): event-mongodb 연결 코드 추가

--- event-server/src/app.module.ts
+++ event-server/src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 전체 앱에서 ConfigService 사용 가능
       envFilePath: process.env.NODE_ENV === 'local' ? '.env.local' : '.env',
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('EVENT_MONGO_URI'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
