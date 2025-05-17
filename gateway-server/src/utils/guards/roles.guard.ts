76dbd15c85f289fd5e4e806351c54a0351709fd3
jxmen <sprnd645@gmail.com>
2025-05-17 22:53:35 +0900

    refactor(gateway): 컨트롤러에서 자주 쓰는 부분 requireRoles 데코레이터로 추출

--- gateway-server/src/utils/guards/roles.guard.ts
+++ gateway-server/src/utils/guards/roles.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../roles';
+import { ROLES_KEY } from '../decorators/roles';
 import { JwtUser } from '../../auth/types/jwt-user.type';
 
 @Injectable()
