0681bfcad4e760ea630e7901fef4416667a4716a
jxmen <sprnd645@gmail.com>
2025-05-17 23:16:03 +0900

    feat(event): Nest 어플리케이션에서 마이크로서비스로 변경

--- event-server/src/main.ts
+++ event-server/src/main.ts
@@ -1,14 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(ConfigService);
+  const appContext = await NestFactory.createApplicationContext(AppModule);
+  const configService = appContext.get(ConfigService);
   const port = configService.get<number>('PORT');
 
-  await app.listen(port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      logger: new ConsoleLogger({
+        prefix: 'Event',
+        logLevels: ['verbose', 'debug', 'log', 'warn', 'error'],
+      }),
+      options: {
+        host: '0.0.0.0',
+        port,
+      },
+    },
+  );
+
+  await app.listen();
 }
 
 bootstrap();
