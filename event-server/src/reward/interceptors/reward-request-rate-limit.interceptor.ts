f93030a294f0650b5de259019eecc7b2944cc778
jxmen <sprnd645@gmail.com>
2025-05-20 02:56:49 +0900

    chore: 보상 요청 rate limit 주석 내용 보충

--- event-server/src/reward/interceptors/reward-request-rate-limit.interceptor.ts
+++ event-server/src/reward/interceptors/reward-request-rate-limit.interceptor.ts
@@ -27,7 +27,7 @@ export class RewardRequestRateLimitInterceptor implements NestInterceptor {
     const eventCode = payload.event_code;
     const userId = payload.user_id;
 
-    // 최초 요청은 eventCode:userId 형태로 키값을 저장합니다. 한번에 너무 많은 요청이 들어오면 예외를 던집니다.
+    // 최초 요청은 eventCode:userId 형태로 키 값을 저장합니다. 한번에 여러번 요청이 들어오면 예외를 던집니다.
     const key = `reward-request-rate-limit:${eventCode}:${userId}`;
     const allowed = await this.redisService.set(key, '1');
     if (!allowed) {
