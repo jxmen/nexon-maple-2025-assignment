b7dff47488792e95495cf3be765bf2b586ec4a33
jxmen <sprnd645@gmail.com>
2025-05-20 06:49:18 +0900

    Revert "refactor: 불필요한 class-validator 어노테이션 제거"
    This reverts commit e03a115b915ae0cf561d2dcc11512e00fad07ac3.

--- apps/event-server/src/reward/dto/get-me-reward-requests.request.ts
+++ apps/event-server/src/reward/dto/get-me-reward-requests.request.ts
@@ -1,7 +1,15 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { RewardRequestLogStatus } from '../reward-request-log.schema';
 
 export class GetMeRewardRequestsRequest {
+  @IsString()
   user_id: string;
+
+  @IsOptional()
+  @IsString()
   event_code?: string;
+
+  @IsOptional()
+  @IsEnum(RewardRequestLogStatus)
   status?: RewardRequestLogStatus;
 }
--- apps/event-server/src/reward/dto/get-reward-requests.request.ts
+++ apps/event-server/src/reward/dto/get-reward-requests.request.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { RewardRequestLogStatus } from '../reward-request-log.schema';
 
 export class GetRewardRequestsRequest {
+  @IsOptional()
+  @IsString()
   event_code?: string;
+
+  @IsOptional()
+  @IsEnum(RewardRequestLogStatus)
   status?: RewardRequestLogStatus;
 }
