7b73f04801b59e6261919f2ec715b4236b1ee1ac
jxmen <sprnd645@gmail.com>
2025-05-20 06:29:32 +0900

    Revert "refactor: 불필요한 파이프 사용 로직 제거"
    This reverts commit 188b7acc6376e973773af4c820c59670c4432d14.

--- apps/gateway-server/src/main.ts
+++ apps/gateway-server/src/main.ts
@@ -1,12 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 값은 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 값이 있으면 에러
+      transform: true, // string → number, boolean, Date 등 타입 변환
+      exceptionFactory: (errors) => {
+        const messages = errors.map(
+          (e) => `${e.property} - ${Object.values(e.constraints).join(', ')}`,
+        );
+
+        return new BadRequestException(messages);
+      },
+    }),
+  );
   await app.listen(port);
 }
 bootstrap();
