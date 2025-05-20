e9ac121ad4ab7eb838427237c63e08fc9a770f38
jxmen <sprnd645@gmail.com>
2025-05-20 14:51:58 +0900

    fix(gateway): 이벤트 생성 API start_date < end_date 충족하도록 설정

--- apps/gateway-server/src/event/dto/create-event-request.ts
+++ apps/gateway-server/src/event/dto/create-event-request.ts
@@ -1,12 +1,40 @@
+// is-start-before-end.validator.ts
 import {
   IsDate,
   IsEnum,
   IsNotEmpty,
   IsObject,
   IsString,
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 import { EventType } from '../enums/event-type';
+
+function IsStartBeforeEnd(
+  property: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      name: 'isStartBeforeEnd',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          if (!value || !relatedValue) return true;
+
+          return value < relatedValue;
+        },
+      },
+    });
+  };
+}
 
 export class CreateEventRequest {
   @IsString()
@@ -25,6 +53,9 @@ export class CreateEventRequest {
 
   @Type(() => Date)
   @IsDate()
+  @IsStartBeforeEnd('end_date', {
+    message: '이벤트 시작 시간은 종료 시간 전이여야 합니다.',
+  })
   start_date: Date;
 
   @Type(() => Date)
